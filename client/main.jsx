6b284216d3fd7e2e7b46160815c9acfef959553a
adamczykjac <adamczyk.jac@gmail.com>
2016-08-19 19:02:43 +0200

    Public stylesheets to be added both to client and server.

--- client/main.jsx
+++ client/main.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
+import { DocHead } from 'meteor/kadira:dochead';
 import { render } from 'react-dom';
 
 import App from '../imports/ui/App.jsx';
 
 Meteor.startup(() => {
+  DocHead.setTitle('Clean Resume - John Doe');
+  // TODO Refactor to iterate over all files
+  // TODO Minify all files with 'juice' from npm
+  var linkInfoMain = {rel: "stylesheet", type: "text/css", href: "stylesheets/main.css"};
+  DocHead.addLink(linkInfoMain);
+  var linkInfoLanguage = {rel: "stylesheet", type: "text/css", href: "stylesheets/languages.css"};
+  DocHead.addLink(linkInfoLanguage);
+  var linkInfoTimeline = {rel: "stylesheet", type: "text/css", href: "stylesheets/timeline.css"};
+  DocHead.addLink(linkInfoTimeline);
+  var linkInfoFontIcons = {rel: "stylesheet", type: "text/css", href: "stylesheets/fonts/icons.css"};
+  DocHead.addLink(linkInfoFontIcons);
+  var linkInfoFontMontserrat = {rel: "stylesheet", type: "text/css", href: "stylesheets/fonts/montserrat.css"};
+  DocHead.addLink(linkInfoFontMontserrat);
+
   render(<App />, document.getElementById('render-target'));
 });
