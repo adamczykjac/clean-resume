dbf9f58a42faf7bbee8ea9aee9d95ecb8a7d2b24
Jacek Adamczyk <adamczyk@PL4MCL-528752.local>
2016-09-12 20:37:04 +0200

    Zmiana dla Pawla.

--- imports/ui/resume-modules/Award.jsx
+++ imports/ui/resume-modules/Award.jsx
@@ -7,8 +7,8 @@ export default class Award extends Component {
       <li>
         <span className="flaticon-award big"></span>
         <div className="award-content">
-          <h4>{ this.props.award.title }</h4>
-          <h5>{ this.props.award.awarder } | { this.props.award.date }</h5>
+          <h4>{ this.props.award.awarder }</h4>
+          <h5>{ this.props.award.title } | { this.props.award.date }</h5>
           <p>{ this.props.award.summary }</p>
         </div>
       </li>
