8a260b53eec64f38ffe9a7fef3fa1de9b7f922ce
Jacek Adamczyk <adamczyk.jac@gmail.com>
2017-02-18 16:45:02 +0100

    Prepared space for landing page.

--- client/main.jsx
+++ client/main.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { Meteor } from 'meteor/meteor';
-import { render } from 'react-dom';
-
-import App from '../imports/ui/App.jsx';
-
-Meteor.startup(() => {
-  render(<App />, document.getElementById('render-target'));
-});
+// import React from 'react';
+// import { Meteor } from 'meteor/meteor';
+// import { render } from 'react-dom';
+//
+// import Landing from '../imports/ui/Landing.jsx';
+//
+// Meteor.startup(() => {
+//   render(<Landing />, document.getElementById('render-target'));
+// });
---
+++ imports/ui/Landing.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class Landing extends Component {
+  render() {
+    return (
+      <div className="top-wrapper">
+        <div className="container">
+          Hello, this is landing page.
+        </div>
+      </div>
+    );
+  }
+}
