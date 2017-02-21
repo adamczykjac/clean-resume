a31a0ac91aa1adf4c6dfccd50b9852b7e13c7090
Jacek Adamczyk <adamczyk.jac@gmail.com>
2017-02-21 22:16:10 +0100

    Landing page - iteration 1.

--- client/main.jsx
+++
@@ -1,9 +0,0 @@
-// import React from 'react';
-// import { Meteor } from 'meteor/meteor';
-// import { render } from 'react-dom';
-//
-// import Landing from '../imports/ui/Landing.jsx';
-//
-// Meteor.startup(() => {
-//   render(<Landing />, document.getElementById('render-target'));
-// });
--- imports/ui/Landing.jsx
+++ imports/ui/Landing.jsx
@@ -3,9 +3,24 @@ import React, { Component } from 'react';
 export class Landing extends Component {
   render() {
     return (
-      <div className="top-wrapper">
-        <div className="container">
-          Hello, this is landing page.
+      <div className="landing">
+        <div className="cleanresume-side-big">
+          <header>
+            <div className="col-sm-6 col-xs-12 slogan">
+              <div className="col-xs-1 timeline-wrapper">
+                <div className="timeline">
+                  <div className="timeline-before"></div>
+                  <div className="timeline-badge primary"><a><div className="circle"></div></a></div>
+                </div>
+              </div>
+              <div className="col-xs-11">
+                <h1>Clean Resume,</h1>
+                <h2>At your service.</h2>
+                <div className="btn btn-lg">Get One</div>
+              </div>
+            </div>
+            <div className="col-sm-6"></div>
+          </header>
         </div>
       </div>
     );
