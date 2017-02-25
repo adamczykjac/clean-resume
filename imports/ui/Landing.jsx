ac3686da1531c71022ebe3a3ad57f95a5c76050d
Jacek Adamczyk <adamczyk.jac@gmail.com>
2017-02-25 23:17:57 +0100

    Name and slogan changed.

--- imports/ui/Landing.jsx
+++ imports/ui/Landing.jsx
@@ -15,16 +15,18 @@ export class Landing extends Component {
               </div>
               <div className="col-md-11 col-xs-1-offset col-xs-10">
                 <div className="slogan-texts">
-                  <h1>Clean Resume,</h1>
-                  <h2>At your service.</h2>
-                  <div className="btn btn-lg">Get One</div>
+                  <h1>Ear<div className="reverse">r</div>ly,</h1>
+                  <h2>The very first impression, done right.</h2>
+                  <h4 className="thinner">We want to beautify your resume, <br />
+                    tell us about your needs.</h4>
+                  <a href="https://goo.gl/forms/ZGd4p1AoSHpjktj53" className="btn btn-lg" target="_blank">Fill the survey</a>
                 </div>
               </div>
             </div>
             <div className="col-sm-6"></div>
           </header>
           <footer className="col-xs-12">
-            &copy; Clean Resume, 2017
+            &copy; earrly.com, 2017
           </footer>
         </div>
       </div>
